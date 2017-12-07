7a622d148fff60592527bce9302564dae94a6c0c
Raza Hussain <mr.raza.hussain@gmail.com>
2017-12-07 22:46:33 +0500

    Added User model, UI gems, devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :channels
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'channels#index'
 end
