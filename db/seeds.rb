7f572b5b64bb7ca25e6c34f2b5521e52258fa425
Raza Hussain <mr.raza.hussain@gmail.com>
2017-12-17 12:20:03 +0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!([{email: 'mr.raza.hussain@gmail.com',password: 'password',password_confirmation: 'password'},{email: 'raiejazahmad@gmail.com',password: 'password',password_confirmation: 'password'}])
+# User.create!([{email: 'mr.raza.hussain@gmail.com',password: 'password',password_confirmation: 'password'},{email: 'raiejazahmad@gmail.com',password: 'password',password_confirmation: 'password'}])
 # CATEGORIES DATA
 
 categories= [
